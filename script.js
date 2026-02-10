46861315db56cc42536acae75e0fc5ec377b80c7
vassiliymarkov <88937545+vassiliymarkov@users.noreply.github.com>
2026-02-10 21:23:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const menu = document.querySelector('.header__menu');
-  const burgerBtn = document.querySelector('.burger');
-  const blackKey = document.querySelector('.burger__black-key');
+  let burgerBtn = document.querySelector('.burger');
 
-  const menuOpenSound = new Audio('sounds/do.mp3');
-  const menuCloseSound = new Audio('sounds/re.mp3');
+  let menuOpenSound = new Audio('/sounds/do.mp3');
+  let menuCloseSound = new Audio('/sounds/re.mp3');
+
+  let burgerBar1 = document.getElementById('burger__bar-1');
+  let burgerBar2 = document.getElementById('burger__bar-2');
+  let burgerBar3 = document.getElementById('burger__bar-3');
+  let mobileMenu = document.querySelector('.header__menu');
+  let body = document.querySelector('.body');
 
   burgerBtn.addEventListener('click', () => {
+    let isOpen = mobileMenu.classList.toggle('open');
+    burgerBar1.classList.toggle('clicked');
+    burgerBar2.classList.toggle('clicked');
+    burgerBar3.classList.toggle('clicked');
+    body.classList.toggle('body-fixed');
+
+    if (isOpen) {
+      menuOpenSound.currentTime = 0;
+      menuOpenSound.play();
+    } else {
+      menuCloseSound.currentTime = 0;
+      menuCloseSound.play();
+    }
+  });
+
+
+});
+
+document.querySelectorAll('.header__menu-link').forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('open');
+    blackKey.classList.remove('down');
+  });
+});
+
+
+
+/*   burgerBtn.addEventListener('click', () => {
     const isOpen = menu.classList.toggle('open');
 
     if (isOpen) {
@@ -19,14 +51,4 @@ document.addEventListener('DOMContentLoaded', () => {
       blackKey.classList.remove('down');
       menuCloseSound.currentTime = 0;
       menuCloseSound.play();
-    }
-  });
-
-  document.querySelectorAll('.header__menu-link').forEach(link => {
-  link.addEventListener('click', () => {
-    menu.classList.remove('open');
-    blackKey.classList.remove('down');
-  });
-});
-
-});
+    } */
